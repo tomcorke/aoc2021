4b3fe3328f8aadbad7bdb73d5c37b083536a82ac
tomcorke <tom@tomcorke.com>
2021-12-09 09:04:13 +0000

    9

--- src/09/index.ts
+++ src/09/index.ts
@@ -1,12 +1,11 @@
 import { readFileSeparated, toNumber, expect } from "../helpers";
 import { Solution } from "..";
-import { runInNewContext } from "vm";
 
 const DAY = "09";
 
 type Input = number[][];
 const parseInput = (values: string[]): Input =>
-  values.map((v) => v.split("").map(toNumber));
+  values.filter((v) => v).map((v) => v.split("").map(toNumber));
 
 const getInput = readFileSeparated("\n", DAY, "input").then(parseInput);
 const getTestInput = readFileSeparated("\n", DAY, "testInput").then(parseInput);
@@ -43,6 +42,20 @@ const processPartOne = (input: Input): number => {
   );
 };
 
+const displayMapWithBasin = (map: number[][], basin: number[][]) => {
+  map.forEach((row, y) => {
+    let line = "";
+    row.forEach((col, x) => {
+      const h = map[y][x];
+      line += basin.some((p) => p[0] === x && p[1] === y)
+        ? `\x1b[1m\x1b[41m${h}\x1b[0m`
+        : h;
+    });
+    console.log(line);
+  });
+  console.log("");
+};
+
 type Coord = number[];
 const processPartTwo = (input: Input): number => {
   const mx = input[0].length - 1;
@@ -57,39 +70,54 @@ const processPartTwo = (input: Input): number => {
     });
   });
 
-  const floodHigher = (
-    x: number,
-    y: number,
-    p: Coord[],
-    lastH: number
-  ): Coord[] => {
-    if (x < 0 || y < 0 || x > mx || y > my) {
-      return [];
-    }
-    if (p.some((pp) => pp[0] === x && pp[1] === y)) {
-      return [];
-    }
-    const h = input[y][x];
-    if (h === 9) {
-      return [];
-    }
-    const np = [...p, [x, y]];
-    return [
-      [x, y],
-      ...floodHigher(x - 1, y - 1, np, h),
-      ...floodHigher(x - 1, y + 1, np, h),
-      ...floodHigher(x + 1, y - 1, np, h),
-      ...floodHigher(x + 1, y + 1, np, h),
-    ];
+  const flood = (x: number, y: number) => {
+    const points: number[][] = [];
+
+    const floodHigher = (x: number, y: number): Coord[] => {
+      if (x < 0 || y < 0 || x > mx || y > my) {
+        return [];
+      }
+      if (points.some((pp) => pp[0] === x && pp[1] === y)) {
+        return [];
+      }
+      const h = input[y][x];
+      if (h >= 9) {
+        return [];
+      }
+      points.push([x, y]);
+      return [
+        [x, y],
+        ...floodHigher(x, y - 1),
+        ...floodHigher(x, y + 1),
+        ...floodHigher(x + 1, y),
+        ...floodHigher(x - 1, y),
+      ];
+    };
+
+    return floodHigher(x, y);
   };
 
+  const basins: number[][][] = [];
   lows.forEach((low) => {
     const [x, y] = low;
-    const h = input[y][x];
-    const basin = floodHigher(x, y, [], h);
-    console.log(basin);
+    const basin = flood(x, y);
+    basins.push(basin);
+    // displayMapWithBasin(input, basin);
   });
-  return 1;
+
+  const basinsBySize = basins.sort((a, b) => b.length - a.length);
+  const topThreeBasins = basinsBySize.slice(0, 3);
+  const basinSum = topThreeBasins.reduce((acc, basin) => acc * basin.length, 1);
+
+  /*
+  const combinedTopBasins = topThreeBasins.reduce(
+    (acc, basin) => [...acc, ...basin],
+    [] as number[][]
+  );
+  displayMapWithBasin(input, combinedTopBasins);
+  */
+
+  return basinSum;
 };
 
 const solution: Solution = async () => {
